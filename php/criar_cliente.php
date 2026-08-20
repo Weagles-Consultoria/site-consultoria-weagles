83038d5b450829317cacd12e121acea5b3f8fd60
Caio Pellegrini <caiopellegrini97@gmail.com>
2026-08-20 16:08:00 -0300

    fix: altera titulo do aviso de grupo para NOVO LEAD RECEBIDO DA IA

--- php/criar_cliente.php
+++ php/criar_cliente.php
@@ -67,7 +67,7 @@ if ($dadosFormulario['dor'] !== '') {
 
 $descricaoTexto .= "*📝 Mensagem:* " . $dadosFormulario['mensagem'];
 
-$mensagemGrupo = "## 📝 NOVO LEAD RECEBIDO\n---\n" . $descricaoTexto;
+$mensagemGrupo = "## 📝 NOVO LEAD RECEBIDO DA IA\n---\n" . $descricaoTexto;
 
 $payload = [
     'nome' => $dadosFormulario['nome'],
